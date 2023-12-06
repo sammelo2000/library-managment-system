c8a47a6fd05eb71b27564f7598177e11457c5e8c
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2023-12-06 10:45:19 +0200

    done setting up express

---
+++ backend/server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config({path: "../server/config.env"})
+
+
+
+
+const corsOptions = {
+    origin: "http://localhost:3000" 
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+mongoose.connect(process.env.ATLAS_URI).then(() => {
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`App is listening on Port:${port}`);
+    })
+}).catch(err => {
+    console.log(err);
+})
+
+app.get("/",(req,res) => {
+    res.status(201).json({message:"Connected to Backend"})
+});
