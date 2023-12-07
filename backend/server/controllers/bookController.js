6cbc4a9ba7d5f01d7b40b1518589a29e22968c39
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2023-12-07 23:35:22 +0200

    getBooks and input requests are working with db

---
+++ backend/server/controllers/bookController.js
@@ -0,0 +1,38 @@
+const Book = require("../models/bookModel");
+const asyncHandler = require("express-async-handler");
+
+
+const inputABook = asyncHandler (async (req,res) => {
+    const {author,title, edition} = req.body;
+    if(!author || !title || !edition) {
+        res.send("Missing field");
+    }
+    const book = new Book({author,title,edition});
+
+  try{  
+    await book.save();
+
+    res.send("Book added");
+    
+
+    }catch(err) {
+        res.send(err);
+    }
+
+}
+);
+
+
+const getAllBooks = asyncHandler (async (req,res) => {
+    try {
+       const  book = await Book.find({});
+       res.send(book);
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+module.exports = {
+    inputABook,
+    getAllBooks
+}
--- backend/server/routes/bookRoute.js
+++ backend/server/routes/bookRoute.js
@@ -1,24 +1,21 @@
 
 const bookRouter = require("express").Router();
+const {inputABook,getAllBooks} = require("../controllers/bookController");
 
-bookRouter.get("/",(req,res) => {
-    res.send("Getting all the books")
-});
+bookRouter.get("/",getAllBooks);
 
 bookRouter.get("/:id",(req,res) => {
     res.send("Get book By ID");
 })
 
-bookRouter.post("/",(req,res) => {
-    res.send("Input a Book");
-})
+bookRouter.post("/",inputABook);
 
 bookRouter.delete("/:id",(req,res) => {
     res.send("Delete by id");
-})
+});
 bookRouter.patch("/:id",(req,res) => {
     res.send("Updated by id");
-})
+});
 
 
 
